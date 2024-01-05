8dadf406731b670887c7c99a3fb40258759b491d
Erleon9 <erosleon9@gmail.com>
2024-01-05 16:33:14 -0700

    Add @mdi/font package and update dependencies

---
+++ src/components/MenuMobile.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div>
+    <q-list>
+      <q-item clickable v-close-popup>
+        <q-item-section>New tab</q-item-section>
+      </q-item>
+      <q-item clickable v-close-popup>
+        <q-item-section>New incognito tab</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable v-close-popup>
+        <q-item-section>Recent tabs</q-item-section>
+      </q-item>
+      <q-item clickable v-close-popup>
+        <q-item-section>History</q-item-section>
+      </q-item>
+      <q-item clickable v-close-popup>
+        <q-item-section>Downloads</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable v-close-popup>
+        <q-item-section>Settings</q-item-section>
+      </q-item>
+      <q-separator />
+      <q-item clickable v-close-popup>
+        <q-item-section>Help &amp; Feedback</q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<style></style>
--- src/components/ToolbarNavbar.vue
+++ src/components/ToolbarNavbar.vue
@@ -11,15 +11,8 @@ const props = defineProps({
     type: String,
     required: true,
   },
-  icon: {
-    type: String,
-    required: true,
-  },
-  buttons: {
-    type: Array,
-    required: true,
-  },
 });
+const showMenu = ref(false);
 
 const isCurrentChildRoute = (childRoute: any) => route.name === childRoute.name;
 const allChildRoutes = router.value.flatMap((mainRoute) =>
@@ -28,21 +21,54 @@ const allChildRoutes = router.value.flatMap((mainRoute) =>
 </script>
 
 <template>
+  <q-dialog v-model="showMenu" v-if="$q.platform.is.mobile">
+    <q-list class="bg-red q-pa-none" style="width: 100%">
+      <q-item
+        clickable
+        v-close-popup
+        v-for="childRoute in allChildRoutes"
+        :key="childRoute.name"
+      >
+        <q-item-section>{{ childRoute.name }}</q-item-section>
+      </q-item>
+    </q-list>
+  </q-dialog>
   <q-toolbar style="background: #14192d">
-    <q-toolbar-title class="text-teal-4 text-h4 q-pa-md text-weight-bolder">
+    <q-toolbar-title
+      :class="
+        $q.platform.is.mobile
+          ? 'text-teal-4 q-pa-md text-weight-bolder'
+          : 'text-teal-4 text-h4 q-pa-md text-weight-bolder'
+      "
+    >
       {{ props.title }}</q-toolbar-title
     >
-    <div v-for="childRoute in allChildRoutes" :key="childRoute.name">
-      <router-link :to="childRoute.path">
-        <q-btn
-          flat
-          :label="childRoute.name"
-          :color="isCurrentChildRoute(childRoute) ? 'teal-1' : 'teal-4'"
-          class="q-mx-sm text-h6 text-weight-bold hoverable"
-        />
-      </router-link>
+    <div v-if="!$q.platform.is.mobile" class="row">
+      <div v-for="childRoute in allChildRoutes" :key="childRoute.name">
+        <router-link :to="childRoute.path">
+          <q-btn
+            flat
+            :label="childRoute.name"
+            :color="isCurrentChildRoute(childRoute) ? 'teal-1' : 'teal-4'"
+            class="q-mx-sm text-h6 text-weight-bold hoverable"
+          />
+        </router-link>
+      </div>
+    </div>
+    <div v-else>
+      <q-btn
+        flat
+        class="text-h6 text-weight-bold hoverable"
+        @click="showMenu = !showMenu"
+      >
+        <q-icon name="menu" color="teal-4" size="2em" />
+      </q-btn>
     </div>
   </q-toolbar>
 </template>
 
-<style></style>
+<style>
+.q-dialog__inner--minimized {
+  padding: 0px !important;
+}
+</style>
